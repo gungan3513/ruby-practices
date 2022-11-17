aca3f1229dd779f14f041f250f8d0a6c1274597b
gansuke <nature.gundem@gmail.com>
2022-11-17 16:47:28 +0900

    課題の内容を再確認し、作り直し

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,20 +1,20 @@
-# 1 ~ 15の間で任意の数字を格納
-number = [*1..15].sample
+# 1 ~ 20の数字を格納
+numbers = [*1..20]
 
-# 3の倍数、5の倍数、それ以外の数字それぞれの処理
-remainder_3 = number % 3
-remainder_5 = number % 5
-answer = ""
+# 1　~　20の数字を条件に沿って表示
+numbers.each do |number|
+    answer = ""
+    if number % 3 == 0
+        answer = "Fizz"
+    end
 
-if remainder_3 == 0
-    answer += "Fizz"
-end
-if remainder_5 == 0
-    answer += "Buzz"
-end
-if answer == ""
-    answer = number
-end
+    if number % 5 == 0
+        answer += "Buzz"
+    end
 
-# 結果を表示
-puts answer
+    if answer == ""
+        answer = number
+    end
+
+    puts answer
+end
