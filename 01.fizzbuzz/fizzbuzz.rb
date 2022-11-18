31a0559a91c4232ddda58517325f431c0d9e1cd4
gansuke <nature.gundem@gmail.com>
2022-11-18 15:14:15 +0900

    コメントの削除、インデント修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,20 +1,18 @@
-# 1 ~ 20の数字を格納
 numbers = [*1..20]
 
-# 1　~　20の数字を条件に沿って表示
 numbers.each do |number|
-    answer = ""
-    if number % 3 == 0
-        answer = "Fizz"
-    end
+answer = ""
+  if number % 3 == 0
+    answer = "Fizz"
+  end
 
-    if number % 5 == 0
-        answer += "Buzz"
-    end
+  if number % 5 == 0
+    answer += "Buzz"
+  end
 
-    if answer == ""
-        answer = number
-    end
+  if answer == ""
+    answer = number
+  end
 
-    puts answer
+  puts answer
 end
